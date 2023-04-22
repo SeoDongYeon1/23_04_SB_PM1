7342ea3aa8418ba9b7e823abb9594c4d9e52b1bf
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-22 14:41:46 +0900

    JSP 연결 후 JSP로 article list 구현

--- src/main/java/com/KoreaIT/sdy/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/sdy/demo/controller/UsrArticleController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,11 +21,30 @@ public class UsrArticleController {
 	private ArticleService articleService;
 	
 	@RequestMapping("/usr/article/list")
-	@ResponseBody
-	public ResultData<List<Article>> showList() {
+	public String showList(Model model) {
 		List<Article> articles = articleService.getArticles();
 		
-		return ResultData.from("S-1", "Article List", articles);
+		model.addAttribute("articles", articles);
+		
+		return "usr/article/list";
+	}
+	
+	@RequestMapping("/usr/article/detail")
+	@ResponseBody
+	public ResultData<Article> showDetail(HttpSession httpSession, String title, String body) {
+		boolean isLogined = false;
+		int loginedMemberId = -1;
+		
+		if(httpSession.getAttribute("loginedMemberId")!=null) {
+			isLogined = true;
+			loginedMemberId = (int)httpSession.getAttribute("loginedMemberId");
+		}
+		
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body, loginedMemberId);
+		
+		Article article = articleService.getArticleById(writeArticleRd.getData1());
+		
+		return ResultData.newData(writeArticleRd, article);
 	}
 	
 	@RequestMapping("/usr/article/doWrite")
--- src/main/java/com/KoreaIT/sdy/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/sdy/demo/controller/UsrHomeController.java
@@ -2,15 +2,18 @@ package com.KoreaIT.sdy.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "ㅎㅇ";
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 	
 }
